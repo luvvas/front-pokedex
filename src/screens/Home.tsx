4c5f181f9cbe54d7d5ece11ac70bd9a7d58a72b3
luvvas <lucasmachado1@ufpr.br>
2023-06-03 20:36:19 -0300

    feat: add home screen static structure

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,72 @@
 import React from 'react'
+import { Text, TextInput, ScrollView, View , FlatList } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
-import { Text, View } from 'react-native';
+import { PokemonCard } from '../components/PokemonCard';
 
-export function Home() {
+import { AntDesign } from '@expo/vector-icons'; 
+
+const DATA = [
+  {
+    title: 'Bulbasaur',
+    id: '#001',
+  },
+  {
+    title: 'Bulbasaur',
+    id: '#002',
+  },
+  {
+    title: 'Bulbasaur',
+    id: '#003',
+  },
+  {
+    title: 'Bulbasaur',
+    id: '#004',
+  },
+  {
+    title: 'Bulbasaur',
+    id: '#005',
+  },
+  {
+    title: 'Bulbasaur',
+    id: '#006',
+  }
+];
+
+
+export function Home({ navigation }) {
   return(
-    <View>
-      <Text>Oi</Text>
-    </View>
+    <ScrollView>
+      <View className='py-12 px-4'>
+        <View className='px-4 mb-4'>
+          <StatusBar style="dark" translucent />
+
+          {/* Title and Description */}
+          <Text className="font-title text-3xl">Pokédex</Text>
+          <Text className="font-body text-xs">Procure pelo nome ou número na Pokédex</Text>
+          
+          {/* Search Bar */}
+          <View className='mt-4 p-2 bg-gray-300 rounded-full'>
+            <View className='left-3 flex-row items-center space-x-3'>
+              <AntDesign name="search1" size={20} color="black" />
+              <TextInput placeholder='Nome ou número'/>
+            </View>
+          </View>
+        </View>
+
+        {/* Pokemon Card */}
+        <View className='overflow-hidden'>
+        <View className='pl-2 top-[-30px]'>
+          {/* FlatList */}
+          <FlatList 
+            data={DATA}
+            renderItem={({item}) => <PokemonCard navigation={navigation} title={item.title} code={item.id}/>}
+            keyExtractor={item => item.id}
+            numColumns={2}
+          />
+        </View>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
