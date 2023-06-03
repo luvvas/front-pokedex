51f641d4d918ca55412186631ee785b6f47b80a1
luvvas <lucasmachado1@ufpr.br>
2023-06-03 20:34:06 -0300

    fix: expo fonts in all app

--- App.js
+++ App.js
@@ -2,13 +2,25 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { Pokemon } from './src/screens/Pokemon'
+import { Home } from './src/screens/Home'
+
+import { Poppins_700Bold, Poppins_700Bold_Italic, Poppins_500Medium, useFonts } from '@expo-google-fonts/poppins'
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Poppins_700Bold, 
+    Poppins_700Bold_Italic, 
+    Poppins_500Medium
+  })
+
+  if(!hasLoadedFonts) return null
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={Pokemon} screenOptions={{headerShown: false}} >
+      <Stack.Navigator initialRouteName={Home} screenOptions={{headerShown: false}} >
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Pokemon" component={Pokemon} />
       </Stack.Navigator>
     </NavigationContainer>
