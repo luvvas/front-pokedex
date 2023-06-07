a85ff647384ae9537aaf097cefcd63da1bb6a2b3
luvvas <lucasmachado1@ufpr.br>
2023-06-07 19:11:59 -0300

    chore: remove tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,6 @@ module.exports = {
         body: 'Poppins_500Medium',
         alt: 'Poppins_700Bold_Italic'
       },
-      colors: {
-        pokemon: {
-          black: '#4b5563',
-          blue: '#60a5fa',
-          brown: '#422006',
-          gray: '#4b5563',
-          green: '#4ade80',
-          pink: '#db2777',
-          purple: '#9333ea',
-          red: '#f87171',
-          white: '#475569',
-          yellow: '#ca8a04',
-        },
-      }
     },
   },
   plugins: [],
