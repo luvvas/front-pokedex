444da5254ffa94d22f7be08f146b42f934c97d8a
luvvas <lucasmachado1@ufpr.br>
2023-06-08 03:12:48 -0300

    refactor: add search bar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { TextInput, View } from "react-native";
+
+import { AntDesign } from '@expo/vector-icons'; 
+
+export function SearchBar({ onSearch }) {
+  const [search, setSearch] = useState('');
+
+  function handleSearch() {
+    onSearch(search)
+  }
+
+  return (
+    <View className='mt-4 p-2 bg-gray-300 rounded-full'>
+      <View className='left-3 flex-row items-center space-x-3'>
+        <AntDesign name="search1" size={20} color="black" />
+        <TextInput 
+          value={search}
+          onChangeText={(text) => setSearch(text)}
+          onSubmitEditing={handleSearch}
+          placeholder='Nome ou número'
+        />
+      </View>
+    </View>
+  )
+}
