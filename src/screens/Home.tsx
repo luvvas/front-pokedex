72ba6b86b0cd134d52ade1e697750d90cfb7e62b
luvvas <lucasmachado1@ufpr.br>
2023-06-09 01:42:22 -0300

    feat: add loading state

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 import { PokemonCard } from '../components/PokemonCard';
 import { SearchBar } from '../components/SearchBar'
 
-import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { api } from '../lib/api'
 
@@ -18,8 +18,8 @@ export function Home({ navigation }) {
   const [pokemonName, setPokemonName] = useState('');
   const [pokemonUrl, setPokemonUrl] = useState('');
 
+  const [loading, setLoading] = useState(true);
   const [currentView, setCurrentView] = useState<'Pokemons' | 'Favoritos'>('Pokemons');
-  const [isClicked, setIsClicked] = useState(true) 
 
   const [offset, setOffset] = useState(0);
 
@@ -27,15 +27,18 @@ export function Home({ navigation }) {
     const response = await api.get(`/pokemon?offset=${page}&limit=10`)
 
     setPokemons(prevPokemons => [...prevPokemons,...response.data.results])
+    setLoading(false)
   }
 
   async function searchPokemons(pokemonName) {
     setPokemons([])
+    setLoading(true)
 
     const response = await api.get(`/pokemon/${pokemonName}`)
     
     setPokemonUrl(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     setPokemonName(response.data.name)
+    setLoading(false)
   }
 
   function handleLoadMore() {
@@ -43,6 +46,7 @@ export function Home({ navigation }) {
     setPokemons([])
     setPokemonName('')
     setPokemonUrl('')
+    setLoading(true);
 
     setOffset(prevOffset => prevOffset + 20)
   }
@@ -52,7 +56,7 @@ export function Home({ navigation }) {
   }, [offset])
   
   // chega uma hora que começa a lagar pq é muito pokemon
-  console.log(favoritePokemon)
+  // console.log(favoritePokemon)
 
   return(
       <ScrollView>
@@ -72,46 +76,52 @@ export function Home({ navigation }) {
             <Pressable onPress={() => setCurrentView('Pokemons')}>
               <Text className='font-title'>Pokemons</Text>
               {currentView === 'Pokemons' && (
-                <View className={`bg-black h-[2px] rounded ${isClicked ? 'block' : 'hidden'}`} />
+                <View className='bg-black h-[2px] rounded block' />
               )}
             </Pressable>
             <Pressable onPress={() => setCurrentView('Favoritos')}>
               <Text className='font-title'>Favoritos</Text>
               {currentView === 'Favoritos' && (
-                <View className={`bg-black h-[2px] rounded ${isClicked ? 'block' : 'hidden'}`} />
+                <View className='bg-black h-[2px] rounded block}' />
               )}
             </Pressable>
           </View>
 
-          {/* Pokemon Card */}
-
-          {currentView === 'Pokemons' && (
-            <View className='overflow-hidden px-4'>
-              <View className='w-auto flex-row flex-wrap top-[-30px] justify-between'>
-                {/* Renders the Pokemons List */}
-                {pokemons.map((pokemon, index) => {
-                  return (
-                    <View key={index}>
-                      <PokemonCard navigation={navigation} title={pokemon.name} url={pokemon.url}/>
-                    </View>
-                  )
-                })}
-
-                {/* Renders the searched Pokemon Name */}
-                {pokemonName && pokemonUrl && (
-                  <View>
-                    <PokemonCard navigation={navigation} title={pokemonName} url={pokemonUrl} />
-                  </View>
-                )}
-              </View>
-
-              <View className='items-center'>
-                <TouchableOpacity className="items-center bg-gray-300 p-4 rounded-full" onPress={handleLoadMore}>
-                  <MaterialCommunityIcons name="pokeball" size={28} color="black" />
-                </TouchableOpacity>
-              </View>
+          {loading ? (
+            <View className='justify-center items-center mt-10'>
+              <Text className='font-title'>Loading</Text>
             </View>
+          ) : (
+            // Pokemon Card
+            currentView === 'Pokemons' && (
+              <View className='overflow-hidden px-4'>
+                <View className='w-auto flex-row flex-wrap top-[-30px] justify-between'>
+                  {/* Renders the Pokemons List */}
+                  {pokemons.map((pokemon, index) => {
+                    return (
+                      <View key={index}>
+                        <PokemonCard navigation={navigation} title={pokemon.name} url={pokemon.url}/>
+                      </View>
+                    )
+                  })}
+  
+                  {/* Renders the searched Pokemon Name */}
+                  {pokemonName && pokemonUrl && (
+                    <View>
+                      <PokemonCard navigation={navigation} title={pokemonName} url={pokemonUrl} />
+                    </View>
+                  )}
+                </View>
+  
+                <View className='items-center'>
+                  <TouchableOpacity className="items-center bg-gray-300 p-4 rounded-full" onPress={handleLoadMore}>
+                    <MaterialCommunityIcons name="pokeball" size={28} color="black" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            )
           )}
+                
 
           {currentView === 'Favoritos' && (
             <View className='overflow-hidden px-4'>
