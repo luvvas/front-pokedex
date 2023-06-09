57324b60f39cb0b8863355afd3168023160e856d
luvvas <lucasmachado1@ufpr.br>
2023-06-09 00:43:49 -0300

    feat: add use context and favorite button view

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { Text, ScrollView, View, TouchableOpacity } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react'
+import { Text, ScrollView, View, TouchableOpacity, Pressable } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import { PokemonCard } from '../components/PokemonCard';
@@ -9,10 +9,18 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { api } from '../lib/api'
 
+import { FavoritePokemonContext } from '../contexts/FavoritesContext';
+
 export function Home({ navigation }) {
-  const [pokemons, setPokemons] = useState([])
-  const [pokemonName, setPokemonName] = useState('')
-  const [pokemonUrl, setPokemonUrl] = useState('')
+  const { favoritePokemon } = useContext(FavoritePokemonContext)
+
+  const [pokemons, setPokemons] = useState([]);
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonUrl, setPokemonUrl] = useState('');
+
+  const [currentView, setCurrentView] = useState<'Pokemons' | 'Favoritos'>('Pokemons');
+  const [isClicked, setIsClicked] = useState(true) 
+
   const [offset, setOffset] = useState(0);
 
   async function listPokemons(page?) {
@@ -44,49 +52,89 @@ export function Home({ navigation }) {
   }, [offset])
   
   // chega uma hora que começa a lagar pq é muito pokemon
-  console.log(pokemonName, pokemonUrl)
+  console.log(favoritePokemon)
 
   return(
-    <ScrollView>
-      <View className='py-12 px-4'>
-        <View className='px-4 mb-4 pt-8'>
-          <StatusBar style="dark" translucent />
+      <ScrollView>
+        <View className='py-12 px-4'>
+          <View className='px-4 mb-4 pt-8'>
+            <StatusBar style="dark" translucent />
 
-          {/* Title and Description */}
-          <Text className="font-title text-3xl">Pokédex</Text>
-          <Text className="font-body text-xs">Procure pelo nome ou número na Pokédex</Text>
-          
-          {/* Search Bar */}
-          <SearchBar onSearch={searchPokemons} />
-        </View>
-
-        {/* Pokemon Card */}
-        <View className='overflow-hidden px-4'>
-          <View className='w-auto flex-row flex-wrap top-[-30px] justify-between'>
-            {/* Renders the Pokemons List */}
-            {pokemons.map((pokemon, index) => {
-              return (
-                <View key={index}>
-                  <PokemonCard navigation={navigation} title={pokemon.name} url={pokemon.url}/>
-                </View>
-              )
-            })}
-
-            {/* Renders the searched Pokemon Name */}
-            {pokemonName && pokemonUrl && (
-              <View>
-                <PokemonCard navigation={navigation} title={pokemonName} url={pokemonUrl} />
-              </View>
-            )}
+            {/* Title and Description */}
+            <Text className="font-title text-3xl">Pokédex</Text>
+            <Text className="font-body text-xs">Procure pelo nome ou número na Pokédex</Text>
+            
+            {/* Search Bar */}
+            <SearchBar onSearch={searchPokemons} />
           </View>
-        </View>
 
-        <View className='items-center'>
-          <TouchableOpacity className="items-center bg-gray-300 p-4 rounded-full" onPress={handleLoadMore}>
-            <MaterialCommunityIcons name="pokeball" size={28} color="black" />
-          </TouchableOpacity>
+          <View className='px-4 flex-row justify-center gap-5'>
+            <Pressable onPress={() => setCurrentView('Pokemons')}>
+              <Text className='font-title'>Pokemons</Text>
+              {currentView === 'Pokemons' && (
+                <View className={`bg-black h-[2px] rounded ${isClicked ? 'block' : 'hidden'}`} />
+              )}
+            </Pressable>
+            <Pressable onPress={() => setCurrentView('Favoritos')}>
+              <Text className='font-title'>Favoritos</Text>
+              {currentView === 'Favoritos' && (
+                <View className={`bg-black h-[2px] rounded ${isClicked ? 'block' : 'hidden'}`} />
+              )}
+            </Pressable>
+          </View>
+
+          {/* Pokemon Card */}
+
+          {currentView === 'Pokemons' && (
+            <View className='overflow-hidden px-4'>
+              <View className='w-auto flex-row flex-wrap top-[-30px] justify-between'>
+                {/* Renders the Pokemons List */}
+                {pokemons.map((pokemon, index) => {
+                  return (
+                    <View key={index}>
+                      <PokemonCard navigation={navigation} title={pokemon.name} url={pokemon.url}/>
+                    </View>
+                  )
+                })}
+
+                {/* Renders the searched Pokemon Name */}
+                {pokemonName && pokemonUrl && (
+                  <View>
+                    <PokemonCard navigation={navigation} title={pokemonName} url={pokemonUrl} />
+                  </View>
+                )}
+              </View>
+
+              <View className='items-center'>
+                <TouchableOpacity className="items-center bg-gray-300 p-4 rounded-full" onPress={handleLoadMore}>
+                  <MaterialCommunityIcons name="pokeball" size={28} color="black" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+
+          {currentView === 'Favoritos' && (
+            <View className='overflow-hidden px-4'>
+              <View className='w-auto flex-row flex-wrap top-[-30px] justify-between'>
+                {/* Renders the Pokemons List */}
+                {favoritePokemon.map((pokemon, index) => {
+                  return (
+                    <View key={index}>
+                      <PokemonCard navigation={navigation} title={pokemon.title} url={pokemon.url}/>
+                    </View>
+                  )
+                })}
+
+                {/* Renders the searched Pokemon Name */}
+                {pokemonName && pokemonUrl && (
+                  <View>
+                    <PokemonCard navigation={navigation} title={pokemonName} url={pokemonUrl} />
+                  </View>
+                )}
+              </View>
+            </View>
+          )}
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
   )
 }
