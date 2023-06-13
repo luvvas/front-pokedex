5ab35d92f0618c5e30bf46ec2370cef1cab1d6f5
luvvas <lucasmachado1@ufpr.br>
2023-06-13 04:28:11 -0300

    refactor: calls to the api changed to pokemon card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -7,6 +7,8 @@ import { PokemonData, PokemonSpecies } from '../types/pokemon';
 
 export function PokemonCard({ navigation, title, url }) {
   const [pokemonData, setPokemonData] = useState<null | PokemonData>(null)
+  const [pokemonSpecies, setPokemonSpecies] = useState<null | PokemonSpecies>(null)
+  const [pokemonEvolutions, setPokemonEvolutions] = useState(null)
   
   const [id, setId] = useState(0)
   const [image, setImage] = useState('')
@@ -22,10 +24,32 @@ export function PokemonCard({ navigation, title, url }) {
     setImage(pokemonData.sprites.other['official-artwork'].front_default)
     
     const responseSpecies = await axios.get<PokemonSpecies>(pokemonData.species.url)
-    const pokemonSpecies = responseSpecies.data
-
-    const darkenedColor = transparentize(0.4, pokemonSpecies.color.name)
+    setPokemonSpecies(responseSpecies.data)
+    const darkenedColor = transparentize(0.4, responseSpecies.data.color.name)
     setColor(darkenedColor)
+
+    const responseEvolution = await axios.get(responseSpecies.data.evolution_chain.url)
+    const allEvolutions = getAllEvolutions(responseEvolution.data)
+    setPokemonEvolutions(allEvolutions)
+  }
+
+  function getAllEvolutions(data) {
+    const evolutions = [];
+
+    function getEvolution(chain) {
+      if(chain.species && chain.species.name) {
+        evolutions.push(chain.species.name)
+      }
+
+      if (chain.evolves_to && chain.evolves_to.length > 0) {
+        chain.evolves_to.forEach((evolution) => {
+          getEvolution(evolution);
+        });
+      }
+    }
+
+    getEvolution(data.chain)
+    return evolutions
   }
 
   useEffect(() => {
@@ -34,7 +58,7 @@ export function PokemonCard({ navigation, title, url }) {
 
 
   return (
-    <TouchableOpacity activeOpacity={0.9} onPress={() => navigation.navigate('Pokemon', { title, url, image, id, color })}>
+    <TouchableOpacity activeOpacity={0.9} onPress={() => navigation.navigate('Pokemon', { title, url, image, id, color, pokemonData, pokemonSpecies, pokemonEvolutions })}>
       <View className="items-center">
         <Image className='w-28 top-[40px] h-28 z-[1]' source={image ? {uri: image} : null} />
         <View style={{ backgroundColor: 'white', elevation: 16, shadowColor: 'black', shadowOpacity: 0.58, shadowOffset: { width: 0, height: 12 }, shadowRadius: 16  }} className="rounded-3xl">
--- src/screens/Pokemon.tsx
+++ src/screens/Pokemon.tsx
@@ -5,57 +5,10 @@ import { StatusBar } from 'expo-status-bar';
 import { Header } from '../components/Header';
 import { ProgressBar } from '../components/ProgressBar';
 
-import axios from 'axios';
-
-import { PokemonData, PokemonSpecies } from '../types/pokemon';
-
 export function Pokemon({ navigation, route }) {
-  const { title, url, image, id, color } = route.params;
-
-  const [pokemonData, setPokemonData] = useState<null | PokemonData>(null)
-  const [pokemonSpecies, setPokemonSpecies] = useState<null | PokemonSpecies>(null)
-  const [pokemonEvolutions, setPokemonEvolutions] = useState(null)
+  const { title, url, image, id, color, pokemonData, pokemonSpecies, pokemonEvolutions } = route.params;
 
   const [currentView, setCurrentView] = useState<'Informations' | 'Abilities' | 'Evolutions'>('Informations')
-
-  async function getPokemon() {
-    const responseData = await axios.get<PokemonData>(url)
-    const pokemonData = responseData.data
-    setPokemonData(pokemonData)
-
-    const responseSpecies = await axios.get<PokemonSpecies>(pokemonData.species.url)
-    const pokemonSpecies = responseSpecies.data
-    setPokemonSpecies(pokemonSpecies)
-
-    const responseEvolution = await axios.get(pokemonSpecies.evolution_chain.url)
-
-    const allEvolutions = getAllEvolutions(responseEvolution.data)
-    setPokemonEvolutions(allEvolutions)
-    // uncheck - growth_rate
-  }
-
-  function getAllEvolutions(data) {
-    const evolutions = [];
-
-    function getEvolution(chain) {
-      if(chain.species && chain.species.name) {
-        evolutions.push(chain.species.name)
-      }
-
-      if (chain.evolves_to && chain.evolves_to.length > 0) {
-        chain.evolves_to.forEach((evolution) => {
-          getEvolution(evolution);
-        });
-      }
-    }
-
-    getEvolution(data.chain)
-    return evolutions
-  }
-
-  useEffect(() => {
-    getPokemon()
-  }, [])
 
   const formattedWeight = pokemonData?.weight && pokemonData.weight / 10;
   const formattedHeight = pokemonData?.height && pokemonData.height / 10;
